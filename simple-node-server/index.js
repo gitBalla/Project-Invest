39ba6d0fc00a4d75214ca848de3e12173dcca3bc
EventBridge <vincentwhitetran@gmail.com>
2022-09-06 15:01:40 +1000

    merge fix

--- simple-node-server/index.js
+++ simple-node-server/index.js
@@ -15,6 +15,12 @@ app.get('/details', (req, res) => {
   res.send({ data: 'Hello World, from express' });
 });
 
+const projects = require('./data/projects.json');
+
+app.get('/projects', (req, res) => {
+  res.json(projects);
+});
+
 app.listen(port, () =>
   console.log(`Hello world app listening on port ${port}!`)
 );
--- simple-react-app/src/App.js
+++ simple-react-app/src/App.js
@@ -4,30 +4,29 @@ import { Navbar, NavbarBrand } from 'reactstrap';
 import Dashboard from './components/dashboard';
 import { PROJECTS } from './shared/projects';
 
-class App extends React.Component {					
-  
-	constructor(props){
-		super(props);
-		
-		this.state = {
-			projects: PROJECTS
-		};
-	}
-	
-	render(){
-		return (
-			<div className='App'>
-				<Navbar color="primary">
-					<div className="container" id="brand">
-						<NavbarBrand>Project-Invest</NavbarBrand>
-            	<button onClick={callApi}>Call API</button>
-					</div>
-				</Navbar>
-				<h1>Project Dashboard</h1>
-				<Dashboard projects={this.state.projects} />
-			</div>
-		);
-	}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      projects: PROJECTS,
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar color="primary">
+          <div className="container" id="brand">
+            <NavbarBrand>Project-Invest</NavbarBrand>
+            <button onClick={callApi}>Call API</button>
+          </div>
+        </Navbar>
+        <h1>Project Dashboard</h1>
+        <Dashboard projects={this.state.projects} />
+      </div>
+    );
+  }
 }
 
 function callApi() {
