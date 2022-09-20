1fccd798a9588f513f811a99f866ee6f704ff614
EventBridge <vincentwhitetran@gmail.com>
2022-09-20 16:14:14 +1000

    get, post project implementation

---
+++ simple-node-server/models/project.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const ProjectSchema = new Schema({
+  name: { type: String, required: true },
+  image: { type: String, required: true },
+  category: { type: String, required: true },
+  description: { type: String, required: true },
+});
+
+// Export model
+module.exports = mongoose.model('Project', ProjectSchema);
