73c5ecd04ada086d08d037ae4bc86333776cb34c
EventBridge <vincentwhitetran@gmail.com>
2022-09-30 14:21:32 +1000

    fix mongodb link

--- simple-node-server/config/dev.js
+++ simple-node-server/config/dev.js
@@ -3,7 +3,7 @@ const config = {
     jwt: 'hehexdbestkey',
   },
   dbUrl:
-    'mongodb://localhostmongodb+srv://admin:0E9rhRUxrrhlPrvC@cluster0.walpx4a.mongodb.net/?retryWrites=true&w=majority:27017/api-design',
+    'mongodb+srv://admin:0E9rhRUxrrhlPrvC@cluster0.walpx4a.mongodb.net/?retryWrites=true&w=majority',
 };
 
 module.exports = config;
