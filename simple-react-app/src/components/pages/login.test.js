5f417a738c4e226198e966dbc6a0f9b5d8bcb3f4
EventBridge <vincentwhitetran@gmail.com>
2022-10-03 21:20:18 +1100

    render login test

---
+++ simple-react-app/src/components/pages/login.test.js
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react';
+import Login from './login';
+import App from '../../App';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  render(
+    <App>
+      <Login />
+      div
+    </App>
+  );
+});
